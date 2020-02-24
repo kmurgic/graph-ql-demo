141ee34132ddb21a27c22784adcf3993705e148b
Keith Murgic <keith.murgic@brandpulse.io>
2020-02-23 21:47:13 -0600

    assign uuids to new tasks

--- src/components/TaskDialog.js
+++ src/components/TaskDialog.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import uuid from 'uuid/v4';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -19,7 +20,7 @@ const TaskDialog = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     submit({
-      id: task?.id || undefined,
+      id: task?.id || uuid(),
       assignee: {
         id: assignee,
         name: users.find(user => user.id === assignee)?.name || '',
